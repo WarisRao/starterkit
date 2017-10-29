5a8887e9193653f7109bb487daa8b4a7be8c3038
Unknown <rao.waris440@gmail.com>
2017-10-30 00:35:28 +0530

    f

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import numeral from 'numeral';
+import './styles.css';
+
+const courseValue = numeral(1000).format('$0,0.00');
+debugger;
+console.log('i would pay ${courseValue} for this awesome course');
